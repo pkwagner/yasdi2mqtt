0b597ead60483ac6df54f2f1d4f1ea0561500021
Paul Wagner <git@alphath.space>
2023-10-13 18:54:27 +0200

    cleanup(mqtt_client): Clean up custom client id PR

--- src/main.c
+++ src/main.c
@@ -24,12 +24,10 @@ int main(int argc, char **argv)
     char *mqtt_user = getenv("MQTT_USER");
     char *mqtt_password = getenv("MQTT_PASSWORD");
 
-    char *mqtt_client_id;
+    char *mqtt_client_id = "yasdi2mqtt";
     if (getenv("MQTT_CLIENT_ID"))
     {
-      mqtt_client_id = getenv("MQTT_CLIENT_ID");
-    } else {
-      mqtt_client_id = "yasdi2mqtt";
+        mqtt_client_id = getenv("MQTT_CLIENT_ID");
     }
 
     int mqtt_qos_level = 2;
@@ -51,10 +49,10 @@ int main(int argc, char **argv)
     log_info("Configuration | MQTT_SERVER = %s", mqtt_server);
     log_info("Configuration | MQTT_PORT = %u", mqtt_port);
     log_info("Configuration | MQTT_SSL_CERT = %s", mqtt_ssl_cert);
+    log_info("Configuration | MQTT_CLIENT_ID = %s", mqtt_client_id);
     log_info("Configuration | MQTT_QOS_LEVEL = %d", mqtt_qos_level);
     log_info("Configuration | MQTT_USER = %s", mqtt_user);
     log_info("Configuration | MQTT_PASSWORD = %s", mqtt_password);
-    log_info("Configuration | MQTT_CLIENT_ID = %s", mqtt_client_id);
 
     if (!mqtt_init(mqtt_server, mqtt_port, mqtt_ssl_cert, mqtt_user, mqtt_password, mqtt_topic_prefix, mqtt_qos_level, mqtt_client_id))
     {
--- src/mqtt_client.h
+++ src/mqtt_client.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-bool mqtt_init(char *server, uint16_t port, char *ssl_cert, char *user, char *password, char *topic_prefix, int qos_level, char *mqtt_client_id);
+bool mqtt_init(char *server, uint16_t port, char *ssl_cert, char *user, char *password, char *topic_prefix, int qos_level, char *client_id);
 void mqtt_destroy();
 bool mqtt_send(char *topic, char *payload);
 
